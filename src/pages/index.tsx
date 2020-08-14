4eea94ff12425f11d1850b2a2820efdf76af812f
marcelo_nogueira <mnsilva@band.com.br>
2020-08-14 20:59:24 -0300

    add support to husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import Head from "next/head";
-import styles from "../../styles/Home.module.css";
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
 
 type Props = {
-  title: string;
-};
+  title: string
+}
 
-export default function Home({ title }: Props) {
+const foo = 'fdsfdsf'
+
+export default function Home({ title = 'React' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,7 +21,7 @@ export default function Home({ title }: Props) {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -60,10 +62,10 @@ export default function Home({ title }: Props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  );
+  )
 }
